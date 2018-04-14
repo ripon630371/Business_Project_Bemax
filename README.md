e80239d30bf0a9b20b532685045429c5f672408a
ripon630371 <riponcmt630371@gmail.com>
2018-04-14 13:59:45 +0600

    new slider and menu add

--- README.md
+++
@@ -1,2 +0,0 @@
-# Business_Project_Bemax
-(4.10.2013) start my project for Bemax
--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@
 "use strict";
 
 
+    $('.slider-active').owlCarousel({
+        loop: true,
+        nav: true,
+        navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+
 
 
 })(jQuery);	

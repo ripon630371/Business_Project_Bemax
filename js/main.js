1f5b6f9a37cc687c3f5e4181d7d2d30d69ea8410
ripon630371 <riponcmt630371@gmail.com>
2018-04-18 23:20:38 +0600

    project slider ok with custom owl-carousel

--- js/main.js
+++ js/main.js
@@ -19,6 +19,22 @@
         }
     })
 
-
+    $('.project_active').owlCarousel({
+        loop: true,
+        nav: true,
+        margin:2,
+        navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
 
 })(jQuery);	

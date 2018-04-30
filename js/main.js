600869683831ac96ae41bdba61c21e23c54d1939
ripon630371 <riponcmt630371@gmail.com>
2018-04-30 20:56:37 +0600

    test blog Posts

--- js/main.js
+++ js/main.js
@@ -38,7 +38,45 @@
             }
         }
     })
-
+     //blog_active active owlCarousel..................
+     $('.blog_active').owlCarousel({
+        loop: true,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+     //brand_active active owlCarousel..................
+     $('.brand_active').owlCarousel({
+        loop: true,
+        dots:false,
+        margin:3,
+        autoplay:true,
+        autoplayTimeout:2000,
+        navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
     //popup_video active magnificPopup..................
     $('.popup_video').magnificPopup({
           type: 'iframe'
